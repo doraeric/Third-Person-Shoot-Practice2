c492afce599269d0f28728450edfc38e3e768430
doraeric <benson.doraemon@gmail.com>
2017-06-07 21:30:51 +0800

    initialize

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	void Start() {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (Cursor.visible == false) {
+				Debug.Log("change to visible");
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+			} else {
+				Debug.Log("change to invisible");
+				Cursor.lockState = CursorLockMode.Locked;
+				Cursor.visible = false;
+			}
+		}
+	}
+}
