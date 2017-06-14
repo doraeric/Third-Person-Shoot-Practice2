769d0e95ed9bac3f4b73a290d58297106b35d139
ShaoEnChen <seanchen47@gmail.com>
2017-06-15 01:04:40 +0800

    add dangerous zone

---
+++ Assets/Scripts/SpherePlayAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpherePlayAudio : MonoBehaviour {
+	public AudioSource audio;
+	public AudioSource bgm;
+	private GameObject player;
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject == player) {
+			bgm.Stop ();
+			audio.Play ();
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject == player) {
+			audio.Stop ();
+			bgm.Play ();
+		}
+	}
+}
